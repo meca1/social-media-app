ea54943836e0bb907c0d1a8dc722b197f179b8c6
Daniel <drramosdev@gmail.com>
2019-12-23 00:52:08 -0500

    Authentication Middleware

--- index.js
+++ index.js
@@ -44,10 +44,44 @@ app.get('/screams', (req, res) => {
     .catch(error => console.log(error));
 });
 
-app.post('/scream', (req, res) => {
+FBAuth = (req, res, next) => {
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  } else {
+    console.log('Not token found');
+    return res.status(403).json({ error: 'Unauthorized' });
+  }
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then(decodedToken => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection('users')
+        .where('userId', '==', req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then(data => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch(err => {
+      console.log('Error while verifying token ', err);
+      return res.status(403).json(error);
+    });
+};
+
+app.post('/scream', FBAuth, (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
+
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createAt: new Date().toISOString()
   };
   db.collection('screams')
