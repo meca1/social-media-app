981ee6da1079d667472bfabb50ecbe83e83847e2
Daniel <drramosdev@gmail.com>
2019-12-23 02:52:18 -0500

    Refactor of all project

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'suer',
+      bosy: 'this is the scream body',
+      createAt: '2019-03-15T11:46:01.018Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+};
